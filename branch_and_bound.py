50323988bdcb675d434af1e2c907a105c82960ab
MxMacambira <ruanmacambira@gmail.com>
2021-08-02 21:37:48 -0300

    branch and bound quase funcionando

---
+++ branch_and_bound.py
@@ -0,0 +1,90 @@
+from dataclasses import InitVar, dataclass, field
+from typing import Collection, Optional
+from simplex import Simplex, print_solution
+from math import floor
+from json import load
+import sys
+
+# breakpoint = lambda: None
+
+Objective = Collection[float]
+Constraints = Collection[Collection[float]]
+
+def node_constraints(index, value, num_constraints):
+    base_value = floor(value)
+    return [
+        [ *( 1 if i == index else 0 for i in range(num_constraints)),  base_value],
+        [ *(-1 if i == index else 0 for i in range(num_constraints)), -base_value -1]
+    ]
+
+@dataclass
+class BABNode:
+    objective: Objective
+    constraints: Constraints
+    simplex: Simplex = field(init=False)
+
+    def __post_init__(self):
+        self.simplex = Simplex(objective=self.objective, constraints=self.constraints)
+
+    def test_node(self):
+        solution = self.simplex.solution()
+
+        if solution is None:
+            return []
+        
+        for index, value in enumerate(solution[1].values()):
+            if not value.is_integer():
+                lt_constraint, gt_constraint = node_constraints(index, value, len(solution[1]))
+                print(value)
+                return [
+                    BABNode(objective=self.objective, constraints = [*self.constraints, lt_constraint]),
+                    BABNode(objective=self.objective, constraints = [*self.constraints, gt_constraint])
+                ]
+        return self
+
+@dataclass
+class BABTree:
+    constraints: Constraints
+    objective: Objective
+    subproblems: Collection =  field(init=False, default_factory=list)
+    candidate_solution: Simplex = field(init=False, default=None)
+
+    def __post_init__(self):
+        start_problem = BABNode(objective=self.objective, constraints=self.constraints)
+        self.subproblems.append(start_problem)
+
+    def solve(self):
+        while len(self.subproblems) > 0:
+            current = self.subproblems.pop(0)
+            result: BABNode = current.test_node()
+            print("subproblems left: ", len(self.subproblems))
+
+            if isinstance(result, list):
+                if len(self.subproblems) > 10:
+                    breakpoint()
+                self.subproblems += result
+            elif isinstance(result, BABNode):
+                if self.candidate_solution is None or \
+                   self.candidate_solution.solution()[0] < result.simplex.solution()[0]:
+                    self.candidate_solution = result.simplex
+        return self.candidate_solution
+
+def branch_and_bound(constraints, objective):
+    solver = BABTree(constraints=constraints, objective=objective)
+    return solver.solve()
+
+def main():
+    if len(sys.argv) < 2:
+        print("Documento não repassado")
+        return
+    with open(sys.argv[1]) as file:
+        model = load(file)
+    solution = branch_and_bound(
+        model['constraints'],
+        model['objective']
+    )
+    if solution is not None:
+        print_solution(solution)
+
+if __name__ == '__main__':
+    main()
--- simplex.py
+++ simplex.py
@@ -1,7 +1,10 @@
 from collections import UserList
-from dataclasses import dataclass
+from dataclasses import InitVar, dataclass, field
 import sys
 from json import load
+from types import SimpleNamespace
+from typing import Collection
+from functools import lru_cache
 
 class Vector(UserList):
     def __mul__(self, other):
@@ -22,12 +25,23 @@ class Vector(UserList):
 
 @dataclass
 class Simplex:
-    matrix: list
-    num_variables: int
-    num_constraints: int
+    constraints: Collection[Collection[float]]
+    objective: Collection[float]
+    matrix: Collection[Collection[float]] = field(init=False)
+    num_variables: int = field(init=False)
+    num_constraints: int = field(init=False)
+    solved: bool = field(init=False, default=False)
+
+    def __post_init__(self):
+        self.matrix = simplex_matrix(self.constraints, self.objective)
+        self.num_constraints = len(self.constraints)
+        self.num_variables = len(self.objective)
 
     def current_solution(self):
         return (self.matrix[0][-1], self._variables())
+
+    def solution(self):
+        return solve_simplex(self)
 
     def _variables(self):
         return dict(((self._varname(key), self._varvalue(key)) for key in range(self.num_variables)))
@@ -42,16 +56,31 @@ class Simplex:
             return self.matrix[index][-1]
         except StopIteration:
             return 0
-        
-
 
 def do_simplex(constraints, objective):
-    matrix = [
+    simplex = Simplex(constraints, objective)
+    return solve_simplex(simplex)
+
+def simplex_matrix(constraints, objective):
+    return [
         normalized_objective(len(constraints), objective),
         *normalized_constraints(constraints)
     ]
-    simplex = Simplex(matrix, len(objective), len(constraints))
-    return solve_simplex(simplex)
+
+def normalized_constraints(constraints):
+    return_value = []
+    for index, constraint in enumerate(constraints):
+        *variables, result = constraint
+        slack = [ 1 if i == index else 0 for i  in range(len(constraints))]
+        return_value.append(Vector([*variables, *slack, result]))
+    return return_value
+
+def normalized_objective(num_constraints, objective):
+    return Vector([
+        *(-x for x in objective),
+        *([0] * num_constraints),
+        0
+    ])
 
 def findindex(iterable, condition):
     return next(
@@ -75,14 +104,19 @@ def solve_simplex(simplex, normalized=set()):
         float(vector[-1]) / vector[target_index] if vector[target_index] != 0 else -1
         for vector in simplex.matrix[1:]
     )
-    ratios = [ratio for ratio in raw_ratios if ratio > 0]
+    ratios = [ratio for ratio in raw_ratios]
     if(len(ratios) == 0):
         print("O sistema possui múltiplas soluções\n")
         return None
 
     minratio_index = ratios.index(min(value for value in ratios if value >= 0)) + 1
     normalized_vector = simplex.matrix[minratio_index]
-    normalized_vector = normalized_vector * (1/float(normalized_vector[target_index]))
+
+    try:
+        normalized_vector = normalized_vector * (1/float(normalized_vector[target_index]))
+    except:
+        breakpoint()
+        raise 'deu ruim'
 
     for index, vector in enumerate(simplex.matrix):
         if index == minratio_index:
@@ -92,21 +126,6 @@ def solve_simplex(simplex, normalized=set()):
     simplex.matrix[minratio_index] = normalized_vector
 
     return solve_simplex(simplex, {*normalized, target_index})
-
-def normalized_constraints(constraints):
-    return_value = []
-    for index, constraint in enumerate(constraints):
-        *variables, result = constraint
-        slack = [ 1 if i == index else 0 for i  in range(len(constraints))]
-        return_value.append(Vector([*variables, *slack, result]))
-    return return_value
-
-def normalized_objective(num_constraints, objective):
-    return Vector([
-        *(-x for x in objective),
-        *([0] * num_constraints),
-        0
-    ])
 
 def print_solution(simplex_solution):
     print(f"Maximun Value: {simplex_solution[0]}")
@@ -123,7 +142,7 @@ def formatted_row(row):
     return ['{:7.3f}'.format(var) for var in row]
 
 def main():
-    if sys.argc < 2:
+    if len(sys.argv) < 2:
         print("Documento não repassado")
         return
     with open(sys.argv[1]) as file:
